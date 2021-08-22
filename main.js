0c79456cea3a6e2150483ffa0479f5b3e3b5bd78
Sirazaza <89315832+Sirazaza@users.noreply.github.com>
2021-08-22 13:18:55 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,42 @@
+// References to DOM elements
+const popcat = document.querySelector("#popcat");
+const btn = document.querySelector("#btn");
+
+// The two images of the POP CAT
+const openMouthImg = "./images/มุกปกติ.jpg";
+const closeMouthImg = "./images/มุกแดง.png";
+
+// The two Popping sounds
+const openMouthSound = new Audio("./sound/sound-open.mp3");
+const closeMouthSound = new Audio("./sound/sound-close.mp3");
+
+// Event Handlers (Desktops)
+btn.addEventListener("mousedown", openMouth);
+btn.addEventListener("mouseup", closeMouth);
+
+// Event Handers (Touch Screens)
+btn.addEventListener("touchstart", function(e) {
+    e.preventDefault();
+    openMouth();
+})
+
+btn.addEventListener("touchend", function(e) {
+    e.preventDefault();
+    closeMouth();
+})
+
+// The functions which will perform the cool stuff
+function openMouth() {
+    popcat.src = openMouthImg;
+    openMouthSound.play();
+}
+
+function closeMouth() {
+    popcat.src = closeMouthImg;
+    closeMouthSound.play();
+}
+var point = 0;
+function pop(){
+    point = point + 1;
+    document.getElementById("point").innerHTML = point
+}
